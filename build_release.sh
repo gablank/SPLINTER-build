d6304f43c2c595595549b1ccc93121591960ec72
Anders Wenhaug <wenhaug@stud.ntnu.no>
2015-06-21 14:15:36 +0200

    Improvements to build script

--- build_release.sh
+++ build_release.sh
@@ -5,33 +5,45 @@ if [ $# -gt 0 ]; then
 	SPLINTER_DIR=$1
 fi
 
+# If the path is not absolute, make it absolute
+if [[ "$SPLINTER_DIR" != /* ]]; then
+	SPLINTER_DIR="$(pwd)/$SPLINTER_DIR"
+fi
+
 mkdir build
 cd build
 
 function build {
-	INSTRUCTION_SET=$1
-	if [ $INSTRUCTION_SET = x86 ]; then
+	ARCH=$1
+	if [ $ARCH = x86 ]; then
 		BITNESS=32
-	elif [ $INSTRUCTION_SET = x64 ]; then
+	elif [ $ARCH = x86-64 ]; then
 		BITNESS=64
 	fi
 
+	COMPILER=$2
+
 	rm -r *
-	echo "Building SPLINTER for $INSTRUCTION_SET with $CXX"
-	cmake "../$SPLINTER_DIR" -DCMAKE_BUILD_TYPE=release -DBITNESS=$BITNESS
-	make -j4
+	echo "Building SPLINTER for $ARCH with $CXX"
+	cmake $SPLINTER_DIR -DCMAKE_BUILD_TYPE=release -DARCH=$ARCH
+	make -j$(nproc)
 	make install
-	cp libsplinter-1-4.so libsplinter-static-1-4.a "../$CXX/$INSTRUCTION_SET"
+	cp libsplinter-1-4.so libsplinter-static-1-4.a "../linux/$COMPILER/$ARCH"
+
+	if [ $COMPILER = "gcc" ]; then
+		cp -r splinter-matlab ../
+	fi
 }
 
-function compress {
+
+function make_release_files {
 	TARGET_FILE=$1
 }
 
 export CXX=$(which g++)
-build x86
-build x64
+build x86 gcc
+build x86-64 gcc
 
 export CXX=$(which clang++-3.5)
-build x86
-build x64
+build x86 clang
+build x86-64 clang
